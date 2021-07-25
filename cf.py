c3427e91851d5227641323a4cf40caec683188fe
Justine Akehurst <justine.akehurst@pm.me>
2021-07-24 18:28:36 -0700

    not much but first commit

---
+++ cf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#from pathlib import Path
+import argparse
+#import sys
+
+#def main():
+#   local_path = Path(".")
+#   print([x for x in local_path.iterdir() if x.is_file()])
+ 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+ 
